75f49616fa3525b142e827c9f546d62af46372d9
Qi Li <liqi17thu@gmail.com>
2021-01-09 18:47:20 +0800

    image load check

--- test.c
+++ test.c
@@ -7,6 +7,8 @@
 #include "fcntl.h"
 #include "fs.h"
 #include "stat.h"
+#include "Loader.h"
+#include "saver.h"
 #include "image_utils.h"
 
 #define PI 3.1415926536
@@ -20,7 +22,7 @@
 
 Window wnd;
 
-RGB* save_icon;
+PBitmap save_icon;
 RGB* delete_icon;
 RGB* cut_icon;
 RGB* pen_icon;
@@ -1243,12 +1245,7 @@ void MsgProc(struct message * msg)
         }
         break;
     case M_MOUSE_MOVE:
-<<<<<<< HEAD
-
-        if(mouse_down == 1 && is_cut==1 && isMouseInCutBox(cutbox_pos.x, cutbox_pos.y)) {
-=======
         if(mouse_down == 1 && is_cut==1 && isCutBoxInContent(cutbox_pos.x, cutbox_pos.y, cutbox_size.w, cutbox_size.h)) {
->>>>>>> 4d482c2d3229ffa65322fcfa75f33a8c5b703448
             lastMousePos.x = mousePos.x;
             lastMousePos.y = mousePos.y;
             mousePos.x = msg->params[0];
@@ -1392,7 +1389,7 @@ void MsgProc(struct message * msg)
     case M_CLOSE_WINDOW:
         // printf(1, save_icon);
         printf(1, "USER_CLOSE\n");
-        free(save_icon);
+        free(save_icon.data);
         free(delete_icon);
         free(cut_icon);
         free(pen_icon);
@@ -1432,7 +1429,7 @@ main(int argc, char *argv[])
     wnd.title = "PhotoViewer";
     
     int h,w;
-    save_icon = malloc(30*30*3);
+    // save_icon = malloc(30*30*3);
     delete_icon = malloc(30*30*3);
     cut_icon = malloc(30*30*3);
     pen_icon = malloc(30*30*3);
@@ -1458,7 +1455,9 @@ main(int argc, char *argv[])
     
     api_createwindow(&wnd);
     
-    read24BitmapFile(save_filename, save_icon, &h, &w);
+
+    save_icon = LoadImg(save_filename);
+    // read24BitmapFile(save_filename, save_icon, &h, &w);
     printf(1, "Icon %d, %d", h, w);
     read24BitmapFile(delete_filename, delete_icon,&h,&w);
     read24BitmapFile(cut_filename, cut_icon, &h, &w);
@@ -1483,7 +1482,7 @@ main(int argc, char *argv[])
     
     // memset(wnd.content, pra * 50, wnd.size.w * wnd.size.h * 3);
 
-    api_drawImgButton(&wnd, save_icon, (Point){0, 0}, (Size){30, 30}, border1, borderColor, normal_shift);
+    api_drawImgButton(&wnd, save_icon.data, (Point){0, 0}, (Size){30, 30}, border1, borderColor, normal_shift);
     api_drawImgButton(&wnd, delete_icon, (Point){30,0},(Size){30,30}, border1, borderColor, normal_shift);
     api_drawImgButton(&wnd, pen_icon, (Point){579,0}, (Size){30,30}, border1, borderColor, normal_shift);
     api_drawImgButton(&wnd, rubber_icon, (Point){610,0}, (Size){30,30}, border1, borderColor, normal_shift);
